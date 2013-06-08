7b738d548e8ff9e2eb3d37f4442f91bf7f167439
eqot <eqobar@gmail.com>
2013-06-08 18:17:38 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LostAndFound::Application.config.secret_key_base = 'a5086117a514a4207c6b4d1c7e74dec29ff58ab60a01140a7dda1e54b430d8f7dae4fbf84fca5eaee75c2a6281425d8e5b5287426bd8ca810e3b559e1b806914'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LostAndFound::Application.config.session_store :cookie_store, key: '_LostAndFound_session'
